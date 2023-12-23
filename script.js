ad3c943701fd277caa5c239f2d40c66c4e04bb81
CodeSandbox <support@codesandbox.io>
2023-12-23 23:30:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,100 @@
+var typed = new Typed(".about__name", {
+  strings: ["ibk", "Ibukun"],
+  typeSpeed: 110,
+  startDelay: 500,
+  backDelay: 5000,
+  backSpeed: 65,
+  // fadeOut: true,
+  loop: true,
+loopCount: 100,
+  // showCursor: false
+  // cursorChar: '...'
+});
+
+const body = document.body;
+
+const btnTheme = document.querySelector(".fa-moon");
+const btnHamburger = document.querySelector(".fa-bars");
+
+const addThemeClass = (bodyClass, btnClass) => {
+  body.classList.add(bodyClass);
+  btnTheme.classList.add(btnClass);
+};
+
+const getBodyTheme = localStorage.getItem("portfolio-theme");
+const getBtnTheme = localStorage.getItem("portfolio-btn-theme");
+
+addThemeClass(getBodyTheme, getBtnTheme);
+
+const isDark = () => body.classList.contains("dark");
+
+const setTheme = (bodyClass, btnClass) => {
+  body.classList.remove(localStorage.getItem("portfolio-theme"));
+  btnTheme.classList.remove(localStorage.getItem("portfolio-btn-theme"));
+
+  addThemeClass(bodyClass, btnClass);
+
+  localStorage.setItem("portfolio-theme", bodyClass);
+  localStorage.setItem("portfolio-btn-theme", btnClass);
+};
+
+const toggleTheme = () =>
+  isDark() ? setTheme("light", "fa-moon") : setTheme("dark", "fa-sun");
+
+btnTheme.addEventListener("click", toggleTheme);
+
+const displayList = () => {
+  const navUl = document.querySelector(".nav__list");
+
+  if (btnHamburger.classList.contains("fa-bars")) {
+    btnHamburger.classList.remove("fa-bars");
+    btnHamburger.classList.add("fa-times");
+    navUl.classList.add("display-nav-list");
+  } else {
+    btnHamburger.classList.remove("fa-times");
+    btnHamburger.classList.add("fa-bars");
+    navUl.classList.remove("display-nav-list");
+  }
+};
+
+btnHamburger.addEventListener("click", displayList);
+
+const scrollUp = () => {
+  const btnScrollTop = document.querySelector(".scroll-top");
+
+  if (body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    btnScrollTop.style.display = "block";
+  } else {
+    btnScrollTop.style.display = "none";
+  }
+};
+
+document.addEventListener("scroll", scrollUp);
+const ball = document.querySelector("div.ball");
+
+let mouseX = 0;
+let mouseY = 0;
+
+let ballX = 0;
+let ballY = 0;
+
+let speed = 0.2;
+
+function animate() {
+  let distX = mouseX - ballX;
+  let distY = mouseY - ballY;
+
+  ballX = ballX + distX * speed;
+  ballY = ballY + distY * speed;
+
+  ball.style.left = ballX + "px";
+  ball.style.top = ballY + "px";
+
+  requestAnimationFrame(animate);
+}
+animate();
+
+document.addEventListener("mousemove", function (event) {
+  mouseX = event.pageX;
+  mouseY = event.pageY;
+});
